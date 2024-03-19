41ca7f7016e248916d00aed28bb290c010b39515
Blake Brazier <blake_brazier@hotmail.com>
2024-03-19 18:31:18 +1100

    Add project files.

---
+++ bmtqs-assessment/Models/ContactModel.cs
@@ -0,0 +1,13 @@
+﻿namespace bmtqs_assessment.Models
+{
+    public class ContactModel
+    {
+        public int ContactID { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Companyname { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? EmailAddress { get; set; }
+    }
+}
